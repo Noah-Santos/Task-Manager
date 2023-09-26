1a526cd4bcbf0db6e2db4d133f4bf6d3f266991c
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2023-09-26 13:49:22 -0700

    Dropdown
    Made the dropdown to be able to select individual task

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const result = document.querySelector('.result');
+const results = document.querySelector('.results');
 
 const fetchTask = async() =>{
     try {
@@ -7,10 +7,12 @@ const fetchTask = async() =>{
 
         // going through the data array and getting the data that holds the value of data
         const task = data.data.map((tasks)=>{
-            return `<h5>${tasks.name}</h5>`;
+            return `<option value="${tasks.name}">${tasks.name}</option>`;
         })
 
-        result.innerHTML = task.join("");
+        results.innerHTML = task.join("");
+
+        change();
     }catch(e){
         // formAlert.textContent = e.response.data.msg;
     }
@@ -22,6 +24,20 @@ const btn = document.querySelector('.submit-btn');
 const input = document.querySelector('.form-input');
 const input2 = document.querySelector('#description');
 const formAlert = document.querySelector('.form-alert');
+let task = document.querySelector('.task');
+let description = document.querySelector('.description');
+
+async function change(){
+    let {data} = await axios.get('/api/task');
+    task.innerHTML = results.value;
+    data.data.find(task =>{
+        console.log(task.name)
+        console.log(task.description)
+        if(task.name == results.value){
+            description.innerHTML = task.description;
+        }
+    })
+}
 
 btn.addEventListener('click', async(event)=>{
     // prevents page from reloading on submit because we are doing something with the data
@@ -30,26 +46,11 @@ btn.addEventListener('click', async(event)=>{
 let nameValue = input.value;
 let descValue = input2.value;
     try{
-        if(!editMode){
-            const {data} = await axios.post('/api/task', {name: nameValue,description:descValue});
-            const h5 = document.createElement('h5');
-            h5.textContent = data.tasks;
-            result.appendChild(h5);
-            fetchTask();
-        }else{
-            console.log(currentId)
-            console.log(nameValue)
-            console.log(descValue);
-            fetch(`/api/task/${currentId}`, {
-                method: "PUT",
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({name: nameValue, description: descValue}),
-            })
-            fetchTask();
-            editMode = false;
-        }
-        
-        // nameAlter();
+        const {data} = await axios.post('/api/task', {name: nameValue,description:descValue});
+        const h5 = document.createElement('h5');
+        h5.textContent = data.tasks;
+        results.appendChild(h5);
+        fetchTask();
     }catch(e){
         console.log(e.response);
         // formAlert.textContent = e.response.data.msg;
@@ -57,15 +58,6 @@ let descValue = input2.value;
     input.value='';
     input2.value='';
 });
-
-var editMode = false;
-var currentId = '';
-
-function nameAlter(name, ids){
-    editMode = true;
-    input.value = name;
-    currentId = ids;
-}
 
 function deleteTask(){
     async(event)=>{
