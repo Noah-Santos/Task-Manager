1e4fdb9cd7d33ab8ab65a76deeb7e7822e9a5fb7
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2023-09-26 14:25:19 -0700

    Edit and Single
    Made the edit to work through the page and got the dropdown to be ironed out

--- public/script.js
+++ public/script.js
@@ -23,41 +23,82 @@ fetchTask();
 const btn = document.querySelector('.submit-btn');
 const input = document.querySelector('.form-input');
 const input2 = document.querySelector('#description');
+const newTask = document.querySelector('#newName');
+const newDesc = document.querySelector('#newDescription');
 const formAlert = document.querySelector('.form-alert');
-let task = document.querySelector('.task');
-let description = document.querySelector('.description');
+const task = document.querySelector('.task');
+const description = document.querySelector('.description');
+let chosenTask = ''; 
+let chosenDescription = '';
+let chosenID;
 
 async function change(){
     let {data} = await axios.get('/api/task');
     task.innerHTML = results.value;
     data.data.find(task =>{
-        console.log(task.name)
-        console.log(task.description)
         if(task.name == results.value){
             description.innerHTML = task.description;
+            sessionStorage.setItem('chosenTask', results.value);
+            sessionStorage.setItem('chosenDescription', task.description);
+            sessionStorage.setItem('chosenID', task.id);
         }
     })
+}
+
+function inputs(){
+    // console.log('yes')
+    newTask.value = sessionStorage.getItem('chosenTask');
+    newDesc.value = sessionStorage.getItem('chosenDescription');
+    chosenID = sessionStorage.getItem('chosenID');
+    // console.log(chosenID);
+    // console.log(sessionStorage.getItem('chosenID'));
+    editMode = true;
 }
 
 btn.addEventListener('click', async(event)=>{
     // prevents page from reloading on submit because we are doing something with the data
     event.preventDefault();
-    
-let nameValue = input.value;
-let descValue = input2.value;
     try{
-        const {data} = await axios.post('/api/task', {name: nameValue,description:descValue});
-        const h5 = document.createElement('h5');
-        h5.textContent = data.tasks;
-        results.appendChild(h5);
-        fetchTask();
+        if(!editMode){
+            let nameValue = input.value;
+            let descValue = input2.value;
+            const {data} = await axios.post('/api/task', {name: nameValue,description:descValue});
+            const h5 = document.createElement('h5');
+            h5.textContent = data.tasks;
+            result.appendChild(h5);
+            fetchTask();
+            input.value='';
+            input2.value='';
+        }else{
+            let taskChange = newTask.value;
+            let descriptionChange = newDesc.value;
+            console.log(taskChange)
+            fetch(`/api/task/${chosenID}`, {
+                method: "PUT",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({name: taskChange, description: descriptionChange}),
+                
+            })
+            newTask.value = '';
+            newDesc.value = '';
+            fetchTask();
+            editMode = false;
+        }
     }catch(e){
         console.log(e.response);
         // formAlert.textContent = e.response.data.msg;
     }
-    input.value='';
-    input2.value='';
 });
+
+var editMode = false;
+var currentId = '';
+
+function nameAlter(){
+    editMode = true;
+    newTask.value = chosenTask;
+    newDesc.value = chosenDescription;
+}
+
 
 function deleteTask(){
     async(event)=>{
