ca4ed49921499e787d5c41a4631d88f281ece177
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2023-09-26 15:16:21 -0700

    Checkbox
    Working on the checkbox part

--- public/all.js
+++ public/all.js
@@ -1,14 +1,16 @@
-const result = document.querySelector('.result');
+const result = document.querySelector('.list');
 
 const fetchTask = async() =>{
     
     try {
         const {data} = await axios.get('/api/task');
         console.log(data);
-
         // going through the data array and getting the data that holds the value of data
         const task = data.data.map((tasks)=>{
-            return `<input type="checkbox" id="${tasks.id}" name="${tasks.id}" value="${tasks.name}" onclick="checked(${tasks.id})"><label for="${tasks.id}">${tasks.name}: ${tasks.description}</label><br>`;
+            return `<form class="row">
+            <div class="grow2"><input type="checkbox" id="item${tasks.id}" name="${tasks.id}" value="${tasks.name}" onclick="checkedTask(${tasks.id})"></div>
+            <div class="grow3"><label for="${tasks.id}"><h3>${tasks.name}: ${tasks.description}</h3></label><br></div>
+            </form>`;
         })
 
         result.innerHTML = task.join("");
@@ -18,9 +20,25 @@ const fetchTask = async() =>{
 }
 fetchTask();
 
-function checked(id){
-    let element = document.getElementById(id)
+function checkedTask(id){
+    let element = document.getElementById(`item${id}`)
     if(element.checked){
-        
+        console.log('item done')
+        // console.log(taskChange)
+        fetch(`/api/task/${id}`, {
+            method: "PUT",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({completed: true}),
+        })
+        element.classList.add('completed');
+    }else if(!element.checked){
+        // console.log('item done')
+        // console.log(taskChange)
+        fetch(`/api/task/${id}`, {
+            method: "PUT",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({completed: false}),
+        })
+        element.classList.remove('completed')
     }
 }
